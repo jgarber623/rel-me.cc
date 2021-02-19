d79d8d7127f1d0a44c3c63de0184b3f57f9196fb
Jason Garber <jason@sixtwothree.org>
2021-02-19 15:33:37 -0500

    Add some failing specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require File.expand_path('../config/environment', __dir__)
 Dir.glob(File.join(Dir.pwd, 'spec', 'support', '**', '*.rb')).sort.each { |f| require f }
 
 RSpec.configure do |config|
+  config.include FixtureHelpers
   config.include RSpecMixin
 
   config.disable_monkey_patching!
