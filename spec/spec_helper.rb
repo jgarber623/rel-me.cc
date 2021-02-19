cc286f3edd1fda37d0363ab0cb2bba9d48edcc33
Jason Garber <jason@sixtwothree.org>
2021-02-19 12:07:29 -0500

    Add initial spec files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'simplecov'
+require 'webmock/rspec'
+
+ENV['RACK_ENV'] = 'test'
+
+require File.expand_path('../config/environment', __dir__)
+
+Dir.glob(File.join(Dir.pwd, 'spec', 'support', '**', '*.rb')).sort.each { |f| require f }
+
+RSpec.configure do |config|
+  config.include RSpecMixin
+
+  config.disable_monkey_patching!
+end
