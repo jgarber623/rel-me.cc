e505c8bc779fdb88c9d1deef2bc2449c1e62126a
Jason Garber <jason@sixtwothree.org>
2022-04-28 14:10:36 -0400

    Add spec_helper and failing 404 spec

---
+++ spec/web/not_found_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe RelMe, roda: :app do
+  describe 'GET /foo' do
+    let(:message) { 'The requested URL could not be found' }
+
+    context 'when requesting text/html' do
+      before do
+        header 'Accept', 'text/html'
+        get '/foo'
+      end
+
+      it { is_expected.to be_not_found }
+      its(:body) { is_expected.to include(message) }
+    end
+
+    context 'when requesting application/json' do
+      before do
+        header 'Accept', 'application/json'
+        get '/foo'
+      end
+
+      it { is_expected.to be_not_found }
+      its(:body) { is_expected.to eq({ message: message }.to_json) }
+    end
+  end
+end
