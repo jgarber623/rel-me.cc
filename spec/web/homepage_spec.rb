d23ebb0596a851cff5649920c4563b413e475393
Jason Garber <jason@sixtwothree.org>
2022-04-28 14:41:27 -0400

    Add failing homepage spec

---
+++ spec/web/homepage_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+RSpec.describe RelMe, roda: :app do
+  describe 'HEAD /' do
+    before { head '/' }
+
+    it { is_expected.to be_ok }
+    its(:body) { is_expected.to be_empty }
+  end
+
+  describe 'GET /' do
+    before { get '/' }
+
+    it { is_expected.to be_ok }
+    its(:body) { is_expected.to include('Enter a URL to get started') }
+  end
+
+  describe 'POST /' do
+    let(:message) { 'The requested method is not allowed' }
+
+    context 'when requesting text/html' do
+      before do
+        header 'Accept', 'text/html'
+        post '/'
+      end
+
+      it { is_expected.to be_method_not_allowed }
+      its(:body) { is_expected.to eq(message) }
+    end
+
+    context 'when requesting application/json' do
+      before do
+        header 'Accept', 'application/json'
+        post '/'
+      end
+
+      it { is_expected.to be_method_not_allowed }
+      its(:body) { is_expected.to eq({ message: message }.to_json) }
+    end
+  end
+end
